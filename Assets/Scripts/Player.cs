82d0f5c548b3e45d6036ba81bfa0e9ac8e53a74a
Huy Tran <htran7228@yahoo.com>
2018-10-23 22:27:05 -0500

    basic movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+   
+    private Rigidbody2D rb;
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+
+    private bool facingRight = true;
+
+
+	// Use this for initialization
+	void Start () {
+
+        
+        rb = GetComponent<Rigidbody2D>();
+
+
+    }
+	
+
+    void FixedUpdate()
+    {
+
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+       
+        if(facingRight == false && moveInput > 0) {
+
+            Flip();
+
+        } else if(facingRight == true && moveInput < 0) {
+
+            Flip();
+
+        }
+
+    }
+
+    void Flip()
+    {
+
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+
+    }
+
+
+
+}
