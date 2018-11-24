b20edaa916203178ee26eff2af501ff1f85d2ae5
Caleb Fung <caleb.t.fung@gmail.com>
2018-11-23 21:37:24 -0600

    color changing mechanic

--- Assets/Spread.cs
+++ Assets/Spread.cs
@@ -1,43 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Spread : MonoBehaviour {
 
 	public GameObject LeftPage;
 	public GameObject RightPage;
 	public Transform SpinePoint; //Center of rotation
+    public GameObject mainCamera;
+    public GameObject playerCamera;
+    public Image background;
+    public GameObject player;
 
-	private enum SpreadState
+    private enum SpreadState
 	{
 		UNOPENED, //both pages on right
 		OPEN, //left page on left, right page on right
 		CLOSED //both pages on left
 	}
 
-	private float rotationStartTime = -1; //This is the number we base our animations off of. When the animation starts we set this to current time.
+    private float rotationStartTime = -1; //This is the number we base our animations off of. When the animation starts we set this to current time.
 	private SpreadState state;
 
+    private float waitCamSwitch; //For when camera switching should take place
+
 	void Start () {
-		LeftPage.transform.RotateAround(SpinePoint.position, Vector3.forward, -180); //Our spread is initially open, so we need to close it (set it to UNOPENED)
+        background.canvasRenderer.SetAlpha(0.0f); //Set alpha here - setting 0.0f from the editor doesn't work
+        background.enabled = true; //Enable background - disabled by default
+
+        LeftPage.transform.RotateAround(SpinePoint.position, Vector3.forward, -180); //Our spread is initially open, so we need to close it (set it to UNOPENED)
 		state = SpreadState.UNOPENED;
-	}
+
+        rotationStartTime = Time.time; //Begin animation
+
+        //Make every PopupGroup pop up
+        PopupGroup[] popUpGroups = GetComponentsInChildren<PopupGroup>();
+        foreach (PopupGroup group in popUpGroups)
+        {
+            group.Pop();
+        }
+    }
 	
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-			rotationStartTime = Time.time; //Begin animation
-
-			//Make every PopupGroup pop up
-			PopupGroup[] popUpGroups = GetComponentsInChildren<PopupGroup>();
-			foreach (PopupGroup group in popUpGroups)
-			{
-				group.Pop();
-			}
-		}
-
 		Animate();
-	}
+        SwitchCamera();
+    }
+
 	void Animate()
 	{
 		//If we set our rotationStartTime then we should be animating
@@ -66,7 +75,40 @@ public class Spread : MonoBehaviour {
 			{
 				state++;
 				rotationStartTime = -1;
-			}
-		}
+
+                waitCamSwitch = Time.time;
+                background.CrossFadeAlpha(0.7f, 0.3f, false); // Fade in
+                player.GetComponent<Rigidbody2D>().simulated = true; //Enable character rigidbody
+
+                // Disable non-default groups
+                foreach (GameObject o in GameObject.FindGameObjectsWithTag("green")) {
+                    if (o.transform.parent.tag != "flip") {
+                        o.SetActive(false);
+                    }
+
+                }
+                foreach (GameObject o in GameObject.FindGameObjectsWithTag("blue")) {
+                    if (o.transform.parent.tag != "flip") {
+                        o.SetActive(false);
+                    }
+                }
+            }
+        }
 	}
+
+    void SwitchCamera()
+    {
+        // Wait 1 second and switch cameras
+        if (Time.time - waitCamSwitch >= 1)
+        {
+            // Switch cameras
+            mainCamera.GetComponent<AudioListener>().enabled = false;
+            mainCamera.SetActive(false);
+            playerCamera.GetComponent<AudioListener>().enabled = true;
+            playerCamera.SetActive(true);
+
+            // Fade out
+            background.CrossFadeAlpha(0.0f, 0.3f, false);
+        }
+    }
 }
