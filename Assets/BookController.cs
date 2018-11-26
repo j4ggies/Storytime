757247426e2b0d9935532d97466bd6e14a6ff4a6
jacobbrunson <jacob@brunson.me>
2018-11-25 23:51:53 -0600

    added camera animation

--- Assets/BookController.cs
+++ Assets/BookController.cs
@@ -7,8 +7,28 @@ public class BookController : MonoBehaviour {
 	public GameObject[] Spreads;
 
 	private int spreadIndex = -1;
-	
+
+	private float startTime = -1;
+	private bool turnedPage = false;
+	private bool zoomedOut = true;
+
+	private Vector3 initialCameraPosition;
+	private Quaternion initialCameraRotation;
+	private Matrix4x4 initialProjectionMatrix;
+
+	private Vector3 desiredCameraPosition = new Vector3(0, 4.5f, -23.8f); //TODO: this X and Y coordinate should follow player
+	private Quaternion desiredCameraRotation = Quaternion.identity;
+	private float orthoSize = 5;
+	private Matrix4x4 desiredProjectionMatrix;
+
 	void Start () {
+		initialCameraPosition = Camera.main.transform.position;
+		initialCameraRotation = Camera.main.transform.rotation;
+		initialProjectionMatrix = Camera.main.projectionMatrix;
+
+		float aspect = Camera.main.aspect;
+		desiredProjectionMatrix = Matrix4x4.Ortho(-orthoSize * aspect, orthoSize * aspect, -orthoSize, orthoSize, 0.3f, 1000f);
+
 		foreach (GameObject obj in Spreads)
 		{
 			obj.transform.position = new Vector3(+8.5f, 0, 0);
@@ -43,12 +63,75 @@ public class BookController : MonoBehaviour {
 
 		spreadIndex++;
 	}
+
+	public void BeginAnimation()
+	{
+		startTime = Time.time;
+	}
 	
 
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Space))
 		{
-			TurnPage();
+			BeginAnimation();
 		}
+
+		Animate();
+	}
+
+	void Animate()
+	{
+		if (startTime < 0)
+		{
+			return;
+		}
+
+		if (zoomedOut)
+		{
+
+			if (!turnedPage)
+			{
+				turnedPage = true;
+				TurnPage();
+			}
+
+			float delta = (Time.time - startTime - 1) * 1f; //Time since animation began
+			float clampedDelta = Mathf.Clamp(delta, 0, 1); //Ensure we are in the range [0, 1]
+
+			Camera.main.transform.position = Vector3.Lerp(initialCameraPosition, desiredCameraPosition, clampedDelta);
+			Camera.main.transform.rotation = Quaternion.Lerp(initialCameraRotation, desiredCameraRotation, clampedDelta);
+			Camera.main.projectionMatrix = MatrixLerp(initialProjectionMatrix, desiredProjectionMatrix, clampedDelta);
+
+			if (delta >= 1)
+			{
+				startTime = -1;
+				zoomedOut = false;
+			}
+		} else
+		{
+			float delta = (Time.time - startTime) * 1f; //Time since animation began
+			float clampedDelta = Mathf.Clamp(delta, 0, 1); //Ensure we are in the range [0, 1]
+
+			Camera.main.transform.position = Vector3.Lerp(desiredCameraPosition, initialCameraPosition, clampedDelta);
+			Camera.main.transform.rotation = Quaternion.Lerp(desiredCameraRotation, initialCameraRotation, clampedDelta);
+			Camera.main.projectionMatrix = MatrixLerp(desiredProjectionMatrix, initialProjectionMatrix, clampedDelta);
+
+			if (delta >= 1)
+			{
+				startTime = Time.time;
+				zoomedOut = true;
+				turnedPage = false;
+			}
+		}
+	}
+
+	Matrix4x4 MatrixLerp(Matrix4x4 from, Matrix4x4 to, float time)
+	{
+		Matrix4x4 ret = new Matrix4x4();
+		for (int i = 0; i < 16; i++)
+		{
+			ret[i] = Mathf.Lerp(from[i], to[i], time);
+		}
+		return ret;
 	}
 }
